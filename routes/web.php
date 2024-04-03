f9255217f1bf5465f70f030e89f30126adf0a59f
motomura1226 <yimt@XyNg>
2024-04-03 23:56:01 +0900

    up

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::get('/posts/{post}', [PostController::class, 'show'])
   
   Route::get('/posts/create', [PostController::class, 'create'])
   ->name('posts.create');
+
+  Route::get('/posts/store', [PostController::class, 'create'])
+  ->name('posts.store');
